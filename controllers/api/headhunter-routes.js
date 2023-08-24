b42eed99c210b783c2bd880fe00de1fac0119095
Camille MacLaren <camillemaclaren@gmail.com>
2023-08-23 21:32:01 -0500

    renamed candidate-routes file, export respective routes
    routes exports: candidate and headhunter for index

--- controllers/api/headhunter-routes.js
+++ controllers/api/headhunter-routes.js
@@ -66,3 +66,5 @@ router.post('/logout', (req, res) => {
         res.status(404).end();
     }
 });
+
+module.exports = router;
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,4 +1,11 @@
 const router = require('express').Router();
 
+const headhunterRoutes = require('./headhunter-routes');
+const candidateRoutes = require('./candidate-routes');
+
+
+router.use('/headhunters', headhunterRoutes);
+router.use('/candidates', candidateRoutes);
+
 
 module.exports = router;
