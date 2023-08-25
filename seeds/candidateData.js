e85b2bd5eec4373598776bd4156d13858a8ffb0c
William-Graves <williamallengraves@gmail.com>
2023-08-24 20:59:01 -0500

    updates to seeds

---
+++ seeds/candidateData.js
@@ -0,0 +1,105 @@
+const {Candidate} = require('../models');
+
+const CAndidateData = [
+
+
+  {
+    
+        
+        name: 'Noah Smith',
+        position:'Tech',
+        username:'Noahsmith',
+        contact:'Noahsmith@email.com',
+        password:'123456',
+    
+    
+    
+    },{
+    
+        
+        name: 'Elijah Williams',
+        position:'Tech',
+        username:'Elijahwilliams',
+        contact:'Elijahwilliams@email.com',
+        password:'verygoodpass',
+    
+    
+    
+    },{
+    
+        
+        name: 'Levi Brown',
+        position:'Tech',
+        username:'Levibrown',
+        contact:'Levibrown@email.com',
+        password:'maximumsecruity',
+    
+    
+    
+    },{
+    
+        
+        name: 'James Jones',
+        position:'Buisness',
+        username:'Jamesjones',
+        contact:'Jamesjones@email.com',
+        password:'heavydutypass',
+    
+    
+    
+    },{
+    
+        
+        name: 'Ethan Garica',
+        position:'Buisness',
+        username:'Ethangarcia',
+        contact:'Ethangarcia@email.com',
+        password:'lockandkey',
+    
+    
+    
+    },{
+    
+        
+        name: 'Samuel Miller',
+        position:'Buisness',
+        username:'Samuelmiller',
+        contact:'Samuelmiller@email.com',
+        password:'shackle',
+    
+    
+    
+    },{
+    
+        
+        name: ' Andrew Davis',
+        position:'Science',
+        username:'Andewdavis',
+        contact:'Andrewdavis@email.com',
+        password:'cuffed4life',
+    
+    
+    
+    },{
+    
+        
+        name: 'Matthew Johnson',
+        position:'Science',
+        username:'Matthewjohnson',
+        contact:'Matthewjohnson@email.com',
+        password:'deathpenalty',
+    
+    
+    
+    },
+
+
+
+]
+
+
+
+
+
+const seedCandidate = () => Candidate.bulkCreate(candidateData);
+module.exports =seedCandidate
---
+++ seeds/headhunterData.js
@@ -0,0 +1,63 @@
+const {Headhunter} = require('../models');
+
+const HeadhunterData = [
+
+{
+    
+    
+    name: 'P. Brain McPuggy',
+    position:'C.E.O',
+    username: 'TheBigDog',
+    contact: 'topdog@Email.com',
+    password: 'Purebred',
+
+
+
+},{
+    
+    
+    name: 'Looney CrazyPants',
+    position: 'Science Industry',
+    username: 'TheProfessor',
+    contact: 'e=mc2@ElementInternals.com',
+    password: 'Boom',
+
+
+
+},{
+    
+    
+    name: 'Hacker Cunningham',
+    position: 'Tech Industry',
+    username: 'Anonymous',
+    contact:'Anon@email.com',
+    password:'Question',
+
+
+
+},{
+    
+
+    name: 'Jo Kerr',
+    position: 'Business Industry',
+    username: 'Mimesnotcrimes',
+    contact: 'Bigshoes@email.com',
+    password:"Honkhonk",
+
+
+
+},
+
+
+
+
+
+
+]
+
+
+
+
+
+const seedHeadhunter = () => Headhunter.bulkCreate(HeadhunterData);
+module.exports =seedHeadhunter
